01fe4484d9b158a525602d3126635d77a507db99
Promise Mukundu <mukundupromise@gmail.com>
2022-11-23 11:02:48 +0200

    routed pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Blog from "./pages/Blog";
+import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="contacts" element={<Contacts />} />
       </Routes>
       <Footer />
     </>
