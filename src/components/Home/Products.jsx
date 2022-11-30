835379bfc69c6e5a7a8069bb67dc8dd2b76d2900
Promise Mukundu <mukundupromise@gmail.com>
2022-11-30 12:11:07 +0200

    added filtered categor

--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -8,6 +8,7 @@ import search from "../../assets/img/icon/search.png";
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [filterCategory, setFilterCategory] = useState();
 
   useEffect(() => {
     axios
@@ -33,67 +34,79 @@ const Products = () => {
             <div className="col-lg-12">
               <ul className="filter__controls">
                 {categories.map((cate) => (
-                  <li className="active" data-filter="*">
+                  <li
+                    className="active"
+                    onClick={() => setFilterCategory(cate)}
+                    data-filter="*"
+                  >
                     {cate.charAt(0).toUpperCase() + cate.slice(1)}
                   </li>
                 ))}
               </ul>
             </div>
-            {products.map((prod) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
-                <div className="product__item">
-                  <div
-                    className="product__item__pic set-bg"
-                    style={{ backgroundImage: `url(${prod.image})` }}
-                  >
-                    <span className="label">{prod.category}</span>
-                    <ul className="product__hover">
-                      <li>
-                        <Link to="#">
-                          <img src={heart} alt="img" />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="#">
-                          <img src={compare} alt="img" />
-                          <span>Compare</span>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="#">
-                          <img src={search} alt="" />
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="product__item__text">
-                    <h6>{prod.title}</h6>
-                    <Link to="#" className="add-cart">
-                      + Add To Cart
-                    </Link>
-                    <div className="rating">
-                      <i className="fa fa-star-o"></i>
-                      <i className="fa fa-star-o"></i>
-                      <i className="fa fa-star-o"></i>
-                      <i className="fa fa-star-o"></i>
-                      <i className="fa fa-star-o"></i>
+            {products
+              .filter((product) => {
+                if (filterCategory) {
+                  return product.category === filterCategory;
+                }
+
+                return true;
+              })
+              .map((prod) => (
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                  <div className="product__item">
+                    <div
+                      className="product__item__pic set-bg"
+                      style={{ backgroundImage: `url(${prod.image})` }}
+                    >
+                      <span className="label">{prod.category}</span>
+                      <ul className="product__hover">
+                        <li>
+                          <Link to="#">
+                            <img src={heart} alt="img" />
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="#">
+                            <img src={compare} alt="img" />
+                            <span>Compare</span>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="#">
+                            <img src={search} alt="" />
+                          </Link>
+                        </li>
+                      </ul>
                     </div>
-                    <h5>${prod.price}</h5>
-                    <div className="product__color__select">
-                      <label for="pc-1">
-                        <input type="radio" id="pc-1" />
-                      </label>
-                      <label className="active black" for="pc-2">
-                        <input type="radio" id="pc-2" />
-                      </label>
-                      <label className="grey" for="pc-3">
-                        <input type="radio" id="pc-3" />
-                      </label>
+                    <div className="product__item__text">
+                      <h6>{prod.title}</h6>
+                      <Link to="#" className="add-cart">
+                        + Add To Cart
+                      </Link>
+                      <div className="rating">
+                        <i className="fa fa-star-o"></i>
+                        <i className="fa fa-star-o"></i>
+                        <i className="fa fa-star-o"></i>
+                        <i className="fa fa-star-o"></i>
+                        <i className="fa fa-star-o"></i>
+                      </div>
+                      <h5>${prod.price}</h5>
+                      <div className="product__color__select">
+                        <label for="pc-1">
+                          <input type="radio" id="pc-1" />
+                        </label>
+                        <label className="active black" for="pc-2">
+                          <input type="radio" id="pc-2" />
+                        </label>
+                        <label className="grey" for="pc-3">
+                          <input type="radio" id="pc-3" />
+                        </label>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       </section>
